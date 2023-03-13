95bad1cf35ae120e6e863c35b4af36e57de8af3d
Nivando <sidvandoni@gmail.com>
2023-03-13 09:44:11 -0300

    fixed start script

--- app.js
+++ app.js
@@ -35,8 +35,6 @@ app.use("/players", player);
 app.use("/clubs", club);
 app.use("/campionships", campionship);
 app.use("/matches", match);
-
-app.get("*", (req, res) => {
-  res.status(400).send({ error: "Incorrect Endpoint" });
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
-module.exports = app;
