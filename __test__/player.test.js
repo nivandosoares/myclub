f048bb3bffed75bbf820ddb07d71b4ec27e4ba57
Nivando <sidvandoni@gmail.com>
2023-03-13 09:44:29 -0300

    fixing npm run dev

--- __test__/player.test.js
+++ __test__/player.test.js
@@ -4,9 +4,8 @@ const request = require("supertest");
 const app = require("../app.js");
 const Player = require("../models/player.model.js");
 
-describe("GET /players", () => {
+describe("GET /players/test", () => {
   it("should return 200 OK", () => {
     return request(app).get("/players").expect(200);
-    expect(res.body.length).toBe(greaterThan(0));
   });
 });
--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require("../app.js");
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
-
-module.exports = { server };
